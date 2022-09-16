f9f3777a06d952d076a7869e102a5a7dbe718cbf
BloodOfPanda <mahlendorf@foxhost.de>
2022-09-16 11:29:43 +0200

    Post Check

--- src/index.php
+++ src/index.php
@@ -33,7 +33,7 @@
         </td>
         </tr>
         <tr>
-          <td style="width: 50%;" colspan="2"><input type="submit" value="Submit"></td>
+          <td style="width: 50%;" colspan="2"><input name="suche" type="submit" value="Submit"></td>
         </tr>
         </tbody>
       </table>
@@ -51,60 +51,63 @@
     // der Umkreis in Km
     $umkreis = htmlspecialchars($_POST["entfernung"], ENT_QUOTES);
 
-    $statement = $db->prepare("SELECT COUNT(*) AS plz_de FROM plz_de WHERE plz = '".$plz."'");
-    $statement->execute();  
-    $anzahl = $statement->fetch();
+    if( isset($_POST['suche']) )
+      {
+        $statement = $db->prepare("SELECT COUNT(*) AS plz_de FROM plz_de WHERE plz = '".$plz."'");
+        $statement->execute();  
+        $anzahl = $statement->fetch();
 
-    if ($anzahl['plz_de'] == "0") {
-      echo "".$_POST["plz"]." ist eine ungültige PLZ!";
-      exit;
-    }
-
-    // Erdradius (geozentrischer Mittelwert) in KM
-    $radius = 6368;
-
-    $statement = $db->prepare("SELECT * FROM plz_de WHERE plz = '".$plz."'");
-    $result = $statement->execute();
-    while($row = $statement->fetch()) {
-      $erg_lon = $row['lon'];
-      $erg_lat = $row['lat'];
-    }
-
-    // Umrechnung von GRAD IN RAD
-    $lon = $erg_lon / 180 * pi();
-    $lat = $erg_lat / 180 * pi();
-
-    // jetzt erfolgt die eigentliche Abfrage
-
-      $statement = $db->prepare("SELECT *, (".$radius." * SQRT(2*(1-cos(RADIANS(lat)) * cos(".$lat.") * (sin(RADIANS(lon)) * sin(".$lon.") + cos(RADIANS(lon)) * cos(".$lon.")) - sin(RADIANS(lat)) * sin(".$lat.")))) AS Distance FROM plz_de WHERE ".$radius." * SQRT(2*(1-cos(RADIANS(lat)) * cos(".$lat.") * (sin(RADIANS(lon)) * sin(".$lon.") + cos(RADIANS(lon)) * cos(".$lon.")) - sin(RADIANS(lat)) * sin(".$lat."))) <= ".$umkreis." ORDER BY Distance ");
-
-    function distance($lat1, $lon1, $lat2, $lon2, $unit) {
-
-      if (($lat1 == $lat2) && ($lon1 == $lon2)) {
-        return 0;
-      }
-      else {
-        $theta = $lon1 - $lon2;
-        $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
-        $dist = acos($dist);
-        $dist = rad2deg($dist);
-        $miles = $dist * 60 * 1.1515;
-        $unit = strtoupper($unit);
-
-        if ($unit == "K") {
-          return ($miles * 1.609344);
-        } else if ($unit == "N") {
-          return ($miles * 0.8684);
-        } else {
-          return $miles;
+        if ($anzahl['plz_de'] == "0") {
+          echo "".$_POST["plz"]." ist eine ungültige PLZ!";
+          exit;
         }
-      }
-    }
 
-    // Ausgabe
-      $result = $statement->execute();
-      while($row = $statement->fetch()) {
-        echo $row['plz']. " - ". $row['ort']. " ".number_format(distance($erg_lat, $erg_lon, $row['lat'], $row['lon'], "K"),1,",","."). " km ". $row['lon']. " - ". $row['lat']. " <br>";
+        // Erdradius (geozentrischer Mittelwert) in KM
+        $radius = 6368;
+
+        $statement = $db->prepare("SELECT * FROM plz_de WHERE plz = '".$plz."'");
+        $result = $statement->execute();
+        while($row = $statement->fetch()) {
+          $erg_lon = $row['lon'];
+          $erg_lat = $row['lat'];
+        }
+
+        // Umrechnung von GRAD IN RAD
+        $lon = $erg_lon / 180 * pi();
+        $lat = $erg_lat / 180 * pi();
+
+        // jetzt erfolgt die eigentliche Abfrage
+
+          $statement = $db->prepare("SELECT *, (".$radius." * SQRT(2*(1-cos(RADIANS(lat)) * cos(".$lat.") * (sin(RADIANS(lon)) * sin(".$lon.") + cos(RADIANS(lon)) * cos(".$lon.")) - sin(RADIANS(lat)) * sin(".$lat.")))) AS Distance FROM plz_de WHERE ".$radius." * SQRT(2*(1-cos(RADIANS(lat)) * cos(".$lat.") * (sin(RADIANS(lon)) * sin(".$lon.") + cos(RADIANS(lon)) * cos(".$lon.")) - sin(RADIANS(lat)) * sin(".$lat."))) <= ".$umkreis." ORDER BY Distance ");
+
+        function distance($lat1, $lon1, $lat2, $lon2, $unit) {
+
+          if (($lat1 == $lat2) && ($lon1 == $lon2)) {
+            return 0;
+          }
+          else {
+            $theta = $lon1 - $lon2;
+            $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
+            $dist = acos($dist);
+            $dist = rad2deg($dist);
+            $miles = $dist * 60 * 1.1515;
+            $unit = strtoupper($unit);
+
+            if ($unit == "K") {
+              return ($miles * 1.609344);
+            } else if ($unit == "N") {
+              return ($miles * 0.8684);
+            } else {
+              return $miles;
+            }
+          }
+        }
+
+        // Ausgabe
+          $result = $statement->execute();
+          while($row = $statement->fetch()) {
+            echo $row['plz']. " - ". $row['ort']. " ".number_format(distance($erg_lat, $erg_lon, $row['lat'], $row['lon'], "K"),1,",","."). " km ". $row['lon']. " - ". $row['lat']. " <br>";
+          }
       }
   ?>
 </body>
