4dfaceab5ec8c6482b22bc647696d8b6609e99dc
BloodOfPanda <mahlendorf@foxhost.de>
2022-09-16 10:02:00 +0200

    Update config.php

--- inc/config.php
+++ inc/config.php
@@ -1,8 +1,8 @@
 <?php
     // Zuerst eine Verbindung zur Datenbank aufbauen!
     $db_host 		    = 'localhost';
-    $db_name 		    = 'foxhost_db2';
-    $db_user 		    = 'foxhost_db2';
-    $db_password 	    = 'y$5LhvaLb&PB';
+    $db_name 		    = 'foxhost_DB';
+    $db_user 		    = 'foxhost_USER';
+    $db_password 	    = 'PASSWORD';
     $db 			    = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_password);
 ?>
